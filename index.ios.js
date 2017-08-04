344233e35feb507d94d6bb633489d6860bb74b4b
caiiiac <caiiiac@163.com>
2017-08-04 16:28:29 +0800

    reset

--- index.ios.js
+++ index.ios.js
@@ -9,37 +9,45 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View,
-  Image
+  View
 } from 'react-native';
-
-class Greeting extends Component {
-	render() {
-		return (
-			<Text>Hello {this.props.name}!</Text>
-		);
-	}
-}
 
 export default class AwesomeProject extends Component {
   render() {
-
-    // //  	图片
-    // let pic = {
-    //   uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    // };
-    // return (
-    //   <Image source={pic} style={{width: 193, height: 110}} />
-    // );
-
-		return (
-    		<View style={{alignItems: 'center'}}>
-    			<Greeting name='Rexxar' />
-    			<Greeting name='Jaina' />
-    			<Greeting name='Valeera' />
-    		</View>
-   		 );
- 	 }
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
