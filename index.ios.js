3fff5ccd1b61f3ae134572dc270a6c1314ef734a
caiiiac <caiiiac@163.com>
2017-08-04 16:25:58 +0800

    Greeting

--- index.ios.js
+++ index.ios.js
@@ -13,18 +13,33 @@ import {
   Image
 } from 'react-native';
 
+class Greeting extends Component {
+	render() {
+		return (
+			<Text>Hello {this.props.name}!</Text>
+		);
+	}
+}
+
 export default class AwesomeProject extends Component {
   render() {
 
-    //  	图片
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
-    return (
-      <Image source={pic} style={{width: 193, height: 110}} />
-    );
+    // //  	图片
+    // let pic = {
+    //   uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    // };
+    // return (
+    //   <Image source={pic} style={{width: 193, height: 110}} />
+    // );
 
-  }
+		return (
+    		<View style={{alignItems: 'center'}}>
+    			<Greeting name='Rexxar' />
+    			<Greeting name='Jaina' />
+    			<Greeting name='Valeera' />
+    		</View>
+   		 );
+ 	 }
 }
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
