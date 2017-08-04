ced748b5951d27cc86bd8c34f835400f0dc4e4d2
caiiiac <caiiiac@163.com>
2017-08-04 17:01:39 +0800

    state

--- index.ios.js
+++ index.ios.js
@@ -12,42 +12,43 @@ import {
   View
 } from 'react-native';
 
+class Blink extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { showText: true };
+
+		// 每1000毫秒对showText状态做一次取反操作
+		setInterval(() => {
+			this.setState(previousState => {
+				return {
+					showText: !previousState.showText
+				};
+			});
+		}, 1000);
+	}
+
+	render() {
+		//根据当前showText的值决定是否显示text的内容
+		let display = this.state.showText ? this.props.text : ' ';
+		return (
+			<Text>{display}</Text>
+		);
+	}
+}
+
 export default class AwesomeProject extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+    	<View>
+    		<Blink text='I love to blink' />
+    		<Blink text='Yes blinking is so great' />
+    		<Blink text='Why did they ever take this out of HTML' />
+    		<Blink text='Look at me look at me look at me' />
+    	</View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
