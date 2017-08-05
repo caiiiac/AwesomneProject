b8f67569b3d9d876f86147e9af527b5c3f158c76
caiiiac <caiiiac@163.com>
2017-08-05 15:12:59 +0800

    flexbox

--- index.ios.js
+++ index.ios.js
@@ -15,7 +15,18 @@ import {
 export default class AwesomeProject extends Component {
   render() {
     return (
-      
+    	// flexDirection:row column
+    	// justifyContent:flex-start center flex-end space-around space-between
+    	<View style={{
+    		flex: 1, 
+    		flexDirection: 'column',
+    		justifyContent: 'space-around',
+    		alignItems: 'flex-end',
+    	}} >
+    		<View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+    		<View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+    		<View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+    	</View>
     );
   }
 }
