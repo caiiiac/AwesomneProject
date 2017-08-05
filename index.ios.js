f18b376275828c725691edfada8e513506e3aa38
caiiiac <caiiiac@163.com>
2017-08-05 10:08:54 +0800

    styleSheet

--- index.ios.js
+++ index.ios.js
@@ -15,10 +15,25 @@ import {
 export default class AwesomeProject extends Component {
   render() {
     return (
-      
+     	<View>
+     	 	<Text style={styles.red}>just red</Text>
+     	 	<Text style={styles.bigblue}>just bigblue</Text>
+     	 	<Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+     	  	<Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+     	</View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+	bigblue: {
+		color: 'blue',
+		fontWeight: 'bold',
+		fontSize: 30,
+	},
+	red: {
+		color: 'red',
+	},
+});
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
