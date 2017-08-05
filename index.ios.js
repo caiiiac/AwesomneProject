f1ba85f7253cfd3e83572a89ff4a065ed19699ff
caiiiac <caiiiac@163.com>
2017-08-05 16:24:24 +0800

    TextInput

--- index.ios.js
+++ index.ios.js
@@ -7,16 +7,32 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
+  TextInput,
   Text,
   View
 } from 'react-native';
 
 export default class AwesomeProject extends Component {
-  render() {
-    return (
-      
-    );
+	constructor(props) {
+		super(props);
+		this.state = {text: ''};
+	}
+
+  	render() {
+    	return (
+    		<View style={{padding: 10}} >
+    			<TextInput 
+    				style={{height: 40}}
+    				placeholder="Type here to translate!"
+    				onChangeText={(text) => this.setState({text})}
+    			/>
+    			<Text style={{padding: 10, fontSize: 42}}>
+    				{this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+    			</Text>
+
+    		</View>
+      	
+    	);
   }
 }
 
