79d5c902f49c43a85367b8f4ac2550a2f50b3fbb
caiiiac <caiiiac@163.com>
2017-08-07 19:59:06 +0800

    FlatList

--- index.ios.js
+++ index.ios.js
@@ -9,16 +9,42 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  FlatList
 } from 'react-native';
 
 export default class AwesomeProject extends Component {
   render() {
     return (
+    	<View>
+    		<FlatList
+    			data={[
+    				{key: 'Devin'},
+    				{key: 'Jackson'},
+    				{key: 'James'},
+    				{key: 'Joel'},
+    				{key: 'John'},
+    				{key: 'Jillian'},
+    				{key: 'Jimmy'},
+    				{key: 'Julie'},
+    			]}
+    			renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+    		/>
+    	</View>
       
     );
   }
 }
-
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
