4a6a9fdb2509cb52e05c6f4735029eaf3b34e888
caiiiac <caiiiac@163.com>
2017-08-08 08:59:18 +0800

    SectionList

--- index.ios.js
+++ index.ios.js
@@ -9,16 +9,55 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  SectionList
 } from 'react-native';
 
 export default class AwesomeProject extends Component {
   render() {
     return (
+    	<View style={styles.container}>
+    		<SectionList
+    			sections={[
+    				{title: 'D', data: [{key: 'Devin'}]},
+    				{title: 'J', data: [
+    				{key: 'Jackson'}, 
+    				{key: 'James'}, 
+    				{key: 'Jillian'}, 
+    				{key: 'Jimmy'}, 
+    				{key: 'Joel'}, 
+    				{key: 'John'}, 
+    				{key: 'Julie'}
+    				]},
+    			]}
+    			renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+    			renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+    		/>
+    	</View>
       
     );
   }
 }
 
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingTop: 22
+	},
+	sectionHeader: {
+		paddingTop: 2,
+		paddingLeft: 10,
+		paddingRight: 10,
+		paddingBottom: 2,
+		fontSize: 14,
+		fontWeight: 'bold',
+		backgroundColor: 'rgba(247,247,247,1.0)',
+	},
+	item: {
+		padding: 10,
+		fontSize: 18,
+		height: 44,
+	},
+})
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
