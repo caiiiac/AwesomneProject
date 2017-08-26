209c26c25c4d9407b0c99b1f106d41598022b1b7
caiiiac <caiiiac@163.com>
2017-08-26 11:28:07 +0800

    ImportantNews

--- index.ios.js
+++ index.ios.js
@@ -23,6 +23,15 @@ export default class AwesomeProject extends Component {
     		<List title='上海市民称墓地太贵买不起 买房存骨灰'></List>
             <List title='朝鲜再发视频:摧毁青瓦台 一切化作灰烬'></List>
             <List title='生活大爆炸人物原型都好牛逼'></List>
+
+            <ImportantNews
+                news={[
+                '解放军报报社大楼正在拆除 标识已被卸下(图)',
+                '韩国停签东三省52家旅行社 或为阻止朝旅游创汇',
+                '南京大学生发起亲吻陌生人活动 有女生献初吻-南京大学生发起亲吻陌生人活动 有女生献初吻',
+                '防总部署长江防汛:以防御98年量级大洪水为目标'
+                ]}>
+            </ImportantNews>
     	</View>
     );
   }
@@ -33,6 +42,38 @@ class List extends Component {
 		return(
 			<View style={styles.list_item}>
 				<Text style={styles.list_item_font}>{this.props.title}</Text>
+			</View>
+		);
+	}
+}
+
+class ImportantNews extends Component {
+	show(title) {
+		alert(title);
+	}
+
+	render() {
+		var news = [];
+		for (var i in this.props.news) {
+			var text = (
+				<View style={styles.list_item} key={i}>
+					<Text 
+						onPress={this.show.bind(this, this.props.news[i])}
+						numberOfLines={2}
+						style={styles.list_item_font}
+						key={i}>
+
+						{this.props.news[i]}
+					</Text>
+				</View>
+			);
+			news.push(text);
+		}
+
+		return(
+			<View style={styles.flex}>
+				<Text style={styles.news_title}>今日要闻</Text>
+				{news}
 			</View>
 		);
 	}
