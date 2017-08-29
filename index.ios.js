49ce1ea50095d9ca68bbf45e05cc2544e4d04221
caiiiac <caiiiac@163.com>
2017-08-29 17:27:31 +0800

    picker

--- index.ios.js
+++ index.ios.js
@@ -10,21 +10,46 @@ import {
   StyleSheet,
   Text,
   View,
-  PixelRatio
+  PixelRatio,
+  Picker
 } from 'react-native';
 
 var onePT = 1 / PixelRatio.get();
 
 export default class AwesomeProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      language: null
+    };
+  }
+
   render() {
     return (
+      <View style={[styles.flex, {marginTop:45}]}>
+        <Text>Picker组件</Text>
+        <Picker
+          selectedValue={this.state.language}
+          onValueChange={lang => this.setState({language:lang})}
+          >
+
+        <Picker.Item label="Java" value="java" />
+        <Picker.Item label="swift" value="swift" />
+        <Picker.Item label="PHP" value="PHP" />
+        <Picker.Item label="Python" value="Python" />
+        </Picker>
+
+        <Text>{this.state.language}</Text>
+      </View>
       
     );
   }
 }
 
 const styles = StyleSheet.create({
-  
+  flex: {
+    flex: 1,
+  },
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
