bc930c2726ee438e4f033a9d385cee6f5380903c
caiiiac <caiiiac@163.com>
2017-08-29 15:09:44 +0800

    touchable

--- index.ios.js
+++ index.ios.js
@@ -10,23 +10,51 @@ import {
   StyleSheet,
   Text,
   View,
-  PixelRatio
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableWithoutFeedback
 } from 'react-native';
 
-var onePx = 1 / PixelRatio.get();
 
 
 export default class AwesomeProject extends Component {
   	render() {
     	return (
+    		<View style={styles.flex}>
+    			<TouchableHighlight onPress={this.show.bind(this, 'TouchableHighlight控件')}
+    				underlayColor='#E1F6FF'>
+    				<Text style={styles.item}>TouchableHighlight控件</Text>
+    			</TouchableHighlight>
 
+    			<TouchableOpacity onPress={this.show.bind(this, 'TouchableOpacity控件')}>
+    				<Text style={styles.item}>TouchableOpacity控件</Text>
+    			</TouchableOpacity>
+
+    			<TouchableWithoutFeedback onPress={this.show.bind(this, 'TouchableWithoutFeedback控件')}>
+    				<Text style={styles.item}>TouchableWithoutFeedback控件</Text>
+    			</TouchableWithoutFeedback>
+
+    		</View>
       
     	);
+  	}
+
+  	show(txt) {
+  		alert(txt);
   	}
 }
 
 const styles = StyleSheet.create({
-  
+	flex: {
+		flex: 1,
+		marginTop: 25,
+	},
+	item: {
+		fontSize: 18,
+		color: '#434343',
+		marginLeft: 5,
+		marginTop: 10,
+	},
 
 });
 
