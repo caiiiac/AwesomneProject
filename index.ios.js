e2f012b068b9cccc21afe9e6c7fa4356e5a80c94
caiiiac <caiiiac@163.com>
2017-08-31 17:01:42 +0800

    test

--- index.ios.js
+++ index.ios.js
@@ -6,21 +6,63 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  PixelRatio
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    FlatList
 } from 'react-native';
 
-var onePT = 1 / PixelRatio.get();
 
-export default class AwesomeProject extends Component {
-  render() {
-    return (
-      
+
+export default class AwesomeProject extends React.PureComponent {
+    state = {selected: (new Map(): Map<string, boolean>)};
+
+    _keyExtractor = (item, index) => item.id;
+
+    _onPressItem = (id: string) => {
+        this.setState((state) => {
+            const selected = new Map(state.selected);
+            selected.set(id, !selected.get(id));
+            return {selected};
+        });
+    }
+
+    _renderItem = ({item}) => (
+        <FlatListItem
+            id={item.id}
+            onPressItem={this._onPressItem}
+            selected={!!this.state.selected.get(item.id)}
+            title={item.title}
+        />
     );
-  }
+
+    render() {
+        return (
+            <FlatList
+                data={this.props.data}
+                extraData={this.state}
+                keyExtractor={this._keyExtractor}
+                renderItem={this._renderItem}
+            />
+      
+        );
+    }
+}
+
+class FlatListItem extends React.PureComponent {
+    _onPress = () => {
+        this.props.onPressItem(this.props.id);
+    };
+
+    render() {
+        return(
+            <SomeOtherWidget
+                {...this.props}
+                onPress={this._onPress}
+            ></SomeOtherWidget>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
