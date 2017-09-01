dd45a68c18e1e3b0a98025e9425f788c72044fe8
caiiiac <caiiiac@163.com>
2017-09-01 17:04:18 +0800

    ios

--- index.ios.js
+++ index.ios.js
@@ -6,25 +6,115 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  PixelRatio
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableOpacity
 } from 'react-native';
+import SideMenu from 'react-native-side-menu';
+import Menu from "./Menu";
 
-var onePT = 1 / PixelRatio.get();
+const image = require('./assets/menu.png');
 
 export default class AwesomeProject extends Component {
-  render() {
-    return (
-      
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false,
+            selectedItem: '',
+        };
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen,
+        });
+    }
+
+    updateMenuState(isOpen) {
+        this.setState({ isOpen });
+    }
+
+    onMenuItemSelected = (item) =>
+        this.setState({
+            isOpen: false,
+            selectedItem: item,
+        });
+
+
+    render() {
+
+
+        const menu = <Menu onItemSelected={this.onMenuItemSelected} />;
+
+        return (
+            <SideMenu
+                menu={menu}
+                isOpen={this.state.isOpen}
+                onChange={(isOpen) => this.updateMenuState(isOpen)}
+            >
+                <View style={styles.container}>
+                    <Text style={styles.welcome}>
+                        Welcome to React Native!
+                    </Text>
+                    <Text style={styles.instructions}>
+                        To get started, edit index.android.js
+                    </Text>
+                    <Text style={styles.instructions}>
+                        Press Cmd+R to reload, {'\n'}
+                        Cmd+Control+Z for dev menu
+                    </Text>
+                    <Text style={ styles.instructions }>
+                        当前选中的菜单是: {this.state.selectedItem}
+                    </Text>
+                </View>
+
+                <TouchableOpacity
+                    onPress={this.toggle}
+                    style={styles.button}
+                >
+                    <Image
+                        source={image}
+                        style={{ width: 32, height: 32  }}
+                    />
+                </TouchableOpacity>
+            </SideMenu>
+        );
+    }
 }
 
+
 const styles = StyleSheet.create({
-  
+    button: {
+        position: 'absolute',
+        top: 20,
+        padding: 10,
+    },
+    caption: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        alignItems: 'center',
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
