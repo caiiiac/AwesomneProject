9643db1f004a11abee389dffaa6328f144d2bbc0
caiiiac <caiiiac@163.com>
2017-09-02 19:11:29 +0800

    获取照片

--- index.ios.js
+++ index.ios.js
@@ -5,26 +5,85 @@
  */
 
 import React, { Component } from 'react';
+
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  PixelRatio
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    ScrollView,
+    Image,
+    CameraRoll
 } from 'react-native';
 
-var onePT = 1 / PixelRatio.get();
+let fetchParams = {
+    first: 10,
+    assetType: 'Photos',
+};
 
-export default class AwesomeProject extends Component {
-  render() {
-    return (
-      
-    );
-  }
+class AwesomeProject extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            images: []
+        };
+    }
+
+    render() {
+        return (
+            <ScrollView>
+                <View style={styles.imageGrid}>
+                    {
+                        this.state.images.map((image) =>
+                            <Image
+                                style={styles.image}
+                                resizeMode='stretch'
+                                source={image}
+                                key={image.uri}
+                        />
+                    )
+                }
+                </View>
+
+            </ScrollView>
+        );
+    }
+
+    componentDidMount() {
+        CameraRoll.getPhotos(fetchParams)
+            .then(
+                (data) => {
+                    console.log(data);
+                    let edges = data.edges;
+                    let images = edges.map((edge) => {
+                        return edge.node.image;
+                    });
+                    this.setState({
+                        images: images,
+                    });
+                }
+            )
+            .catch(error => {
+                console.log('发生错误:' + error);
+            });
+    }
+
+
 }
 
 const styles = StyleSheet.create({
-  
+
+    imageGrid: {
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+    },
+    image: {
+        width: 100,
+        height: 100,
+        margin: 10,
+    },
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
