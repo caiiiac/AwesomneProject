83ef868e5ec6d4e7c0f2d0de83663a76e7cd383a
caiiiac <caiiiac@163.com>
2017-09-09 17:38:44 +0800

    Modal

--- index.ios.js
+++ index.ios.js
@@ -10,21 +10,45 @@ import {
     StyleSheet,
     Text,
     View,
-    PixelRatio
+    PixelRatio,
+    Modal
 } from 'react-native';
 
 var onePT = 1 / PixelRatio.get();
 
 export default class AwesomeProject extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            animationType: 'none',
+            modalVisible: false,
+            transparent: true,
+        };
+    }
+
     render() {
         return (
-      
+            <View style={{ alignItems: 'center', flex: 1 }}>
+                <Text
+                    style={styles.upText}
+                    onpress={ this._setModalVisible.bind(this, true)}
+                >检查更新</Text>
+            </View>
         );
+    }
+
+    _setModalVisible = (visible) => {
+        this.setState({ modalVisible: visible });
     }
 }
 
 const styles = StyleSheet.create({
-  
+    upText: {
+        fontSize: 28,
+        color: 'red',
+        marginTop: 30,
+    }
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
