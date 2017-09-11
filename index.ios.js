67a1d369a08c5b5813833cac16a621ab17521d94
caiiiac <caiiiac@163.com>
2017-09-11 17:13:48 +0800

    Link

--- index.ios.js
+++ index.ios.js
@@ -10,21 +10,70 @@ import {
     StyleSheet,
     Text,
     View,
-    PixelRatio
+    Linking,
+    TouchableHighlight
 } from 'react-native';
 
-var onePT = 1 / PixelRatio.get();
 
 export default class AwesomeProject extends Component {
+    componentDidMount() {
+        var url = Linking.getInitialURL().then((url) => {
+            if (url) {
+                console.log('schem:' + url);
+            } else {
+                console.log('schem为空');
+            }
+        }).catch(err => console.error('错误:', err));
+    }
     render() {
         return (
-      
+            <View>
+                <LinkButton url={'https://www.baidu.com'} text="打开百度" />
+            </View>
+        );
+    }
+}
+
+class LinkButton extends  Component {
+    constructor(props) {
+        super(props);
+    }
+
+    static propTypes = {
+        url: React.PropTypes.string,
+        text: React.PropTypes.string.isRequired,
+    }
+
+    render() {
+        return(
+            <TouchableHighlight
+                style={ styles.button }
+                underlayColor='#a5a5a5'
+                onPress={ () => Linking.canOpenURL(this.props.url).then( supported => {
+                    if (supported) {
+                        Linking.openURL(this.props.url);
+                    } else {
+                        console.log('无法打开:' + this.props.url);
+                    }
+                })}
+            >
+                <Text style={ styles.buttonText }>{this.props.text}</Text>
+            </TouchableHighlight>
         );
     }
 }
 
 const styles = StyleSheet.create({
-  
+    button: {
+        margin: 5,
+        backgroundColor: 'white',
+        padding: 15,
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        borderBottomColor: '#cdcdcd',
+    },
+    buttonText: {
+        fontSize: 20,
+    },
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
