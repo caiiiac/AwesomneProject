19c37615a6ff17cec58b382c6824dd046338969e
caiiiac <caiiiac@163.com>
2017-09-14 16:47:25 +0800

    event

---
+++ event.js
@@ -0,0 +1,29 @@
+/**
+ * Created by caiiiac on 2017/9/14.
+ */
+
+let events = {
+
+}
+
+export const on = (name, handler) => {
+    const listeners = events[name];
+    if (!listeners) {
+        events[name] = [];
+    }
+    events[name].push(handler);
+}
+
+export const trigger = (name, data) => {
+    const listeners = events[name];
+    for(let i = 0; i < listeners.length; i++){
+        listeners[i](data);
+    }
+}
+
+export const remove = (name, handler) => {
+    const listeners = events[name];
+    events[name] = listeners.find(
+        v => v!==handler
+    );
+}
