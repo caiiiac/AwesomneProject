1b9988e7d1fe8d5f0a8ffc8c584f7705466364dc
caiiiac <caiiiac@163.com>
2017-09-14 16:35:01 +0800

    init

--- index.ios.js
+++ index.ios.js
@@ -9,22 +9,61 @@ import {
     AppRegistry,
     StyleSheet,
     Text,
-    View,
-    PixelRatio
+    View
 } from 'react-native';
 
-var onePT = 1 / PixelRatio.get();
 
 export default class AwesomeProject extends Component {
     render() {
         return (
-      
+            <View style={ styles.container }>
+                <Counter />
+                <Counter />
+            </View>
         );
     }
 }
 
+class Counter extends  Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0,
+        };
+    }
+
+    render() {
+        return (
+            <View style={styles.row}>
+                <Text style={[ styles.font, styles.counterMargin ]}>计数器:{this.state.counter}</Text>
+                <Text style={ styles.font } onPress={this.addCounter.bind(this)}>点击</Text>
+            </View>
+        );
+    }
+
+    addCounter() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    }
+}
+
 const styles = StyleSheet.create({
-  
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    row: {
+        flexDirection: 'row',
+    },
+    font: {
+        fontSize: 20,
+    },
+    counterMargin: {
+        marginRight: 20,
+    },
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
