95707ed32b47e6bddae75f15be05bc2495ead5f8
caiiiac <caiiiac@163.com>
2017-09-16 15:33:11 +0800

    connector

--- connector.js
+++ connector.js
@@ -1,3 +1,41 @@
 /**
  * Created by caiiiac on 2017/9/15.
  */
+
+
+import React, { Component } from 'react';
+import { on, remove } from './event';
+import { getValue } from './counter';
+
+export const connector = (name, __Component)=> {
+    class SimpleEventConnector extends Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                data: getValue(),
+            };
+
+            this.handler = ( (data) => {
+                this.setState({
+                    data: data
+                });
+            }).bind(this);
+        }
+
+        componentDidMount() {
+            on(name, this.handler);
+        }
+
+        componentWillUnmount() {
+            remove(name, this.handler);
+        }
+
+        render() {
+            const {data} = this.state;
+
+            return <__Component data={data} />
+        }
+    }
+
+    return SimpleEventConnector;
+}
